666eef4e478537e217453518c8bbb01e3078b9e6
BURUKALA MANI REETHIKA <manireethikab@gmail.com>
2026-06-22 21:30:40 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,12 @@ from flask_jwt_extended import (
     jwt_required,
     get_jwt_identity
 )
+from flask_jwt_extended import (
+    JWTManager,
+    create_access_token,
+    jwt_required,
+    get_jwt_identity
+)
 
 from models import db, User, JobApplication
 
@@ -45,3 +51,119 @@ def register():
     return jsonify({
         "message": "User Registered Successfully"
     })
+    @app.route('/login', methods=['POST'])
+def login():
+
+    data = request.json
+
+    user = User.query.filter_by(
+        email=data['email']
+    ).first()
+
+    if not user:
+        return jsonify({
+            "message": "User not found"
+        }), 404
+
+    if bcrypt.check_password_hash(
+        user.password,
+        data['password']
+    ):
+
+        access_token = create_access_token(
+            identity=user.id
+        )
+
+        return jsonify({
+            "token": access_token
+        })
+
+    return jsonify({
+        "message": "Invalid password"
+    }), 401
+    @app.route('/login', methods=['POST'])
+def login():
+
+    data = request.json
+
+    user = User.query.filter_by(
+        email=data['email']
+    ).first()
+
+    if not user:
+        return jsonify({"message": "User Not Found"}), 404
+
+    if bcrypt.check_password_hash(
+        user.password,
+        data['password']
+    ):
+
+        token = create_access_token(
+            identity=user.id
+        )
+
+        return jsonify({
+            "token": token
+        })
+
+    return jsonify({
+        "message": "Invalid Credentials"
+    }), 401
+    @app.route('/add-job', methods=['POST'])
+@jwt_required()
+def add_job():
+
+    current_user = get_jwt_identity()
+
+    data = request.json
+
+    job = JobApplication(
+        company=data['company'],
+        role=data['role'],
+        status=data['status'],
+        user_id=current_user
+    )
+
+    db.session.add(job)
+    db.session.commit()
+
+    return jsonify({
+        "message": "Job Added"
+    })
+    @app.route('/jobs')
+@jwt_required()
+def jobs():
+
+    current_user = get_jwt_identity()
+
+    job_list = JobApplication.query.filter_by(
+        user_id=current_user
+    )
+
+    results = []
+
+    for job in job_list:
+        results.append({
+            "company": job.company,
+            "role": job.role,
+            "status": job.status
+        })
+
+    return jsonify(results)
+    {
+  "username":"reethika",
+  "email":"reethika@gmail.com",
+  "password":"123456"
+}
+    {
+  "email":"reethika@gmail.com",
+  "password":"123456"
+}
+    {
+  "company":"Google",
+  "role":"Python Developer",
+  "status":"Applied"
+}
+app.config["JWT_SECRET_KEY"] = "career_twin_secret_key"
+
+jwt = JWTManager(app)
